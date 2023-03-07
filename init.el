85398bb208f1acb7f265be1e23d7d392fff8813d
dhennigar <dhennigar@protonmail.com>
2023-03-07 13:50:50 -0800

    initial commit

---
+++ init.el
@@ -0,0 +1,46 @@
+;; init.el --- Daniel's init file
+;;; Commentary:
+;; Daniel's init file.
+
+;;; Code:
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+;; use-package declarations
+ (use-package ess
+  :ensure t
+  :init (require 'ess-site)
+  :config (setq ess-use-flymake nil))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode t)
+  :custom (flycheck-lintr-linters
+            "linters_with_defaults(trailing_blank_lines_linter = NULL)"))
+
+(use-package company
+  :init (global-company-mode t))
+
+(use-package nimbus-theme)
+(load-theme 'nimbus t)
+
+;; general settings
+(setq auto-save-default nil)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-fringe-mode 10)
+
+					; (set-face-attribute 'default t :font "Cascadia Code 11")
+
+(if (string-equal system-type "windows-nt")
+    (add-to-list 'default-frame-alist '(font . "Cascadia Code 10" )))
+
