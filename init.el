d3b9a77daaebaa399902af6c02fb88e9ca323a32
dhennigar <dhennigar@protonmail.com>
2023-03-11 11:35:56 -0800

    add rmarkdown support

--- init.el
+++ init.el
@@ -1,16 +1,10 @@
-;;; init.el --- Daniel's init file
-;;; Commentary:
-;;   Only a few packages for R development.
+;; init.el --- Daniel's init file
 
-;;; Code:
-
-;; package repositories
 (require 'package)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; use-package initialization
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -18,10 +12,11 @@
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
 
-;; use-package declarations
 (use-package ess
-;;  :init (require 'ess-site)
   :config (setq ess-use-flymake nil))
+
+(use-package poly-R
+  :ensure t)
 
 (use-package flycheck
   :init (global-flycheck-mode t)
@@ -31,31 +26,26 @@
 (use-package company
   :init (global-company-mode t))
 
-(use-package nimbus-theme
-  :init (load-theme 'nimbus t))
+(use-package tree-sitter
+  :ensure t)
+(use-package tree-sitter-langs
+  :ensure t)
 
 (use-package evil
   :init (evil-mode 1))
 
-;; midnight mode
-(require 'midnight)
-(midnight-delay-set 'midnight-delay "4:30am")
-
-;; set font on windows
 (if (string-equal system-type "windows-nt")
     (add-to-list 'default-frame-alist '(font . "Cascadia Code 10" ))
     (add-to-list 'default-frame-alist '(font . "IBM Plex Mono 11" )))
 
-;; gui settings
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (set-fringe-mode 10)
 
-;; temp and backup files
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-(provide 'init)
-;;; init.el ends here
+(require 'midnight)
+(midnight-delay-set 'midnight-delay "4:30am")
