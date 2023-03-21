1876d34f65444f3349993aae777f5f6f0eeb9055
dhennigar <dhennigar@protonmail.com>
2023-03-21 16:24:02 -0700

    using helm-bibtex

--- lisp/org-mode-settings.el
+++ lisp/org-mode-settings.el
@@ -1,12 +1,14 @@
 ;; org-mode-settings.el
 
 ;; bibtex citations in org-mode
-;;(use-package helm-bibtex
-;;  :custom ((bibtex-completion-bibliography "~/Zotero/zotero.bib") ;; this is not correct for Windows! (Unless I move it)
-;;	   (bibtex-completion-pdf-field "File"))
-;;  )
+(use-package helm-bibtex
+  :custom ((bibtex-completion-bibliography "~/Zotero/bibtex/bibtex.bib")
+	   (bibtex-completion-pdf-field "file")))
 
 ;; org-mode settings
 (with-eval-after-load 'org       
   (setq org-startup-indented t)
   (add-hook 'org-mode-hook #'visual-line-mode))
+
+;; pdf-tools
+(use-package pdf-tools)
