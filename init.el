645a8205bc47b29b06d015e070370fb7e3cc3301
dhennigar <dhennigar@protonmail.com>
2023-06-03 21:50:28 -0700

    got lisp working

--- init.el
+++ init.el
@@ -182,8 +182,11 @@
 (add-hook 'go-mode-hook 'eglot-ensure)
 
 ;; Common Lisp
+
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program "sbcl")
+
 
 
 ;; Org-Mode -----------------------------------------------------
