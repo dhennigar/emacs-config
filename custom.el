dd1371b3bf42adc6db578aaf63f54edd8a2f520c
dhennigar <dhennigar@protonmail.com>
2023-08-05 20:59:35 -0700

    do not use package list in custom.el

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vertico use-package poly-R pkg-info pdf-tools org-contrib orderless nov modus-themes marginalia ess diminish corfu citar-denote calibredb ahk-mode)))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
