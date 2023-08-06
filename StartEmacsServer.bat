30585814c0471d457cf7d01825a3f8781c86cee2
dhennigar <dhennigar@protonmail.com>
2023-08-05 22:39:25 -0700

    non-specific bat file

--- StartEmacsServer.bat
+++ StartEmacsServer.bat
@@ -1,3 +1,3 @@
 del /q C:\Users\dhenn\.emacs.d\server\*
 tasklist /fi "ImageName eq emacs.exe" /fo csv 2>NUL | find /I "emacs.exe">NUL
-if NOT "%ERRORLEVEL%"=="0" "C:\Program Files\Emacs\emacs-29.1\bin\runemacs.exe" --daemon --chdir C:\Users\dhenn
+if NOT "%ERRORLEVEL%"=="0" "C:\Program Files\Emacs\emacs-29.1\bin\runemacs.exe" --daemon --chdir %HOMEPATH%
