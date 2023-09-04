1544d68b769c38605a1bf9efbd4cdd87a7d723a2
dhennigar <danrhennigar@gmail.com>
2023-09-04 11:46:42 -0700

    move native completion to separate file

---
+++ lisp/native-completion.el
@@ -0,0 +1,29 @@
+;;; native-completion.el --- Making the most of the built in completion framework
+;;; Author: Daniel Hennigar
+
+;;; Commentary:
+
+;; This makes the most of the built in completion framework - essentially trying to emulate
+;; packages like Vertico that provide vertical completion buffers.
+
+;;; Code:
+
+(setq completion-auto-help 'always)
+(setq completion-auto-select 'second-tab)
+
+(setq completions-max-height 10)
+(setq completions-header-format nil)
+(setq completion-show-help nil)
+
+(define-key minibuffer-mode-map (kbd "C-n")
+	    #'minibuffer-next-completion)
+(define-key minibuffer-mode-map (kbd "C-p")
+	    #'minibuffer-previous-completion)
+
+(define-key completion-in-region-mode-map (kbd "C-n")
+	    #'minibuffer-next-completion)
+(define-key completion-in-region-mode-map (kbd "C-p")
+	    #'minibuffer-previous-completion)
+
+(provide 'native-completion)
+;;; native-completion.el ends here
