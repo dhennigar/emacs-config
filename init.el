a86cf37f282cd17d30ba18043973cd4793549076
dhennigar <drhenn@proton.me>
2024-02-12 17:33:02 -0800

    superior themes and code aesthetics

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
 ;; -----------------------------------------------------------------------------
 ;; Package Management
 
+;; Package repositories
 (require 'package)
 (add-to-list
  'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
@@ -40,71 +41,96 @@
  'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; Declarative package management
 (require 'use-package)
 (setq use-package-always-ensure t)
 (setq use-package-always-defer t)
 (setq use-package-expand-minimally t)
 
+;; Selectively hide minor modes from the mode line
 (use-package diminish)
 
 
 ;; -----------------------------------------------------------------------------
-;; Theme settings
-
-(use-package modus-themes
-  :custom
-  ;; interface options
-  (modus-themes-org-blocks 'tinted-background)
-  (modus-themes-prompts '(bold))
-  ;; customize colors
-  (modus-themes-common-palette-overrides
-   '((fnname blue-cooler)
-     (string green)
-     (variable cyan)
-     (border-mode-line-active unspecified)
-     (border-mode-line-inactive)))
-  (modus-operandi-palette-overrides
-   '((bg-mode-line-active blue-cooler)
-     (fg-mode-line-active bg-dim)
-     (bg-region bg-blue-subtle)))
-  (modus-vivendi-palette-overrides
-   '((bg-region bg-blue-nuanced))))
-
-;; Set a dark or light theme on startup based on time of day.
-(if (and (< (nth 2 (decode-time (current-time))) 19)
-	 (>= (nth 2 (decode-time (current-time))) 6))
-    (load-theme 'modus-operandi)
-  (load-theme 'modus-vivendi))
-
-;; ;; Export modus themes to various formats.
-;; (require 'modus-themes-exporter)
-
-
-;; -----------------------------------------------------------------------------
-;; OS-Specific Configuration
+;; Linux-specific configuration
 
 (when (eq system-type 'gnu/linux)
-  (use-package vterm)			; shell for linux
+  ;; A much nicer terminal emulator
+  (use-package vterm)
   (use-package vterm-toggle
     :bind
     ("C-c t" . 'vterm-toggle))
+  ;; ctags-esque functionality
   (use-package ggtags
     :bind
     (:map ggtags-mode-map
 	  ("C-c g s" . 'ggtags-find-other-symbol))))
 
-(when (eq system-type 'windows-nt)
-  (use-package powershell))		; shell for windows
-
 
 ;; -----------------------------------------------------------------------------
-;; General
+;; Theme settings
 
+;; Set these transition times (in hours) for time-appropriate theme selection.
+(defvar dh/evening-transition 19)
+(defvar dh/morning-transition 6)
+
+(use-package standard-themes
+  :custom
+  (standard-themes-disable-other-themes t)
+  (standard-themes-mixed-fonts t)
+  (standard-themes-common-palette-overrides
+   '((comment fg-dim)
+     (string fg-added)))
+  :bind
+  ("<f5>" . standard-themes-toggle))
+
+;; Select a time-of-day-appropriate Standard theme.
+(if (and (< (nth 2 (decode-time (current-time))) dh/evening-transition)
+	 (>= (nth 2 (decode-time (current-time))) dh/morning-transition))
+    (standard-themes-load-light)
+  (standard-themes-load-dark))
+
+(defun dh/ef-themes-load-random ()
+    "Select a random time-of-day-appropriate Ef theme."
+    (interactive)
+    (if (and (< (nth 2 (decode-time (current-time))) dh/evening-transition)
+	     (>= (nth 2 (decode-time (current-time))) dh/morning-transition))
+	(ef-themes-load-random 'light)
+      (ef-themes-load-random 'dark)))
+
+(use-package ef-themes
+  :custom
+  (ef-themes-org-blocks 'tinted-background)
+  (ef-themes-mixed-fonts t)
+  (ef-themes-common-palette-overrides
+   '((comment fg-dim)))
+  :bind
+  ("<f6>" . dh/ef-themes-load-random)
+  :config
+  (require 'ef-themes-exporter))
+
+(use-package modus-themes
+  :custom
+  (modus-themes-org-blocks 'tinted-background)
+  (modus-themes-mixed-fonts t)
+  (modus-themes-common-palette-overrides
+   '((comment fg-dim)
+     (string green)
+     (border-mode-line-active unspecified)
+     (border-mode-line-inactive unspecified)))
+  :bind
+  ("<f7>" . modus-themes-toggle)
+  :config
+  (require 'modus-themes-exporter))
+
+;; -----------------------------------------------------------------------------
+;; General navigation and interface settings
+
+;; Buffer navigation
 (bind-key* "C-x <right>" 'windmove-right)
 (bind-key* "C-x <left>"  'windmove-left)
 (bind-key* "C-x <up>"    'windmove-up)
 (bind-key* "C-x <down>"  'windmove-down)
-
 (use-package buffer-move
   :bind
   ("C-x S-<right>" . 'buf-move-right)
@@ -112,27 +138,26 @@
   ("C-x S-<up>" . 'buf-move-up)
   ("C-x S-<down>" . 'buf-move-down))
 
+;; Change some unintuitive keybindings
 (bind-key "C-M-g" 'exit-recursive-edit) ; a more natural exit command
-
 (unbind-key "C-?") ; formerly undo-redo
 (bind-key "C-x M-u" 'undo-redo)
 
-(use-package ctrlf ; more intuitive ISearch, with a nice interface.
+;; Superior search tool
+(use-package ctrlf
   :init
   (ctrlf-mode))
 
+;; Compare files in two buffers
 (use-package ediff
   :custom
   (ediff-split-window-function 'split-window-horizontal)
   (ediff-window-setup-function 'ediff-setup-windows-plain))
 
-
+;; Show available key sequence completions with C-h
 (use-package which-key
   :custom
-  ;; Allow C-h to trigger which-key before it is done automatically
   (which-key-show-early-on-C-h t)
-  ;; make sure which-key doesn't show normally but refreshes quickly after it is
-  ;; triggered.
   (which-key-idle-delay 10000)
   (which-key-idle-secondary-delay 0.05)
   :init
@@ -141,50 +166,53 @@
 ;; -----------------------------------------------------------------------------
 ;; Autocompletion
 
-;; This provides a pretty good completion system using only built in tools.
-;; It isn't as nice as the packages below, but serves as a usable backup.
-;; Note: Does not combine with vertico or corfu, but does work with orderless
-;; and marginalia.
+;; ;; `NATIVE-COMPLETION' provides a pretty good completion system using
+;; ;; only built-in tools. It isn't as nice as the packages below, but serves
+;; ;; as a usable backup.
 ;; (require 'native-completion)
 
+;; Basic autocompletion ergonomics
 (electric-pair-mode)
 (setq tab-always-indent 'complete)
 (setq tab-first-completion nil)
 
+;; Space delimited orderless selection filtering
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides
    '((file (styles basic partial-completion)))))
 
+;; Provide useful details in completion buffers
 (use-package marginalia
   :init (marginalia-mode))
 
+;; Provide completion candidates in a vertical list
 (use-package vertico
+  :custom
+  (vertico-count 8)
   :init (vertico-mode))
 
+;; On-the-fly completion candidate overlay
 (use-package corfu
   :config
   (require 'corfu-popupinfo)
   (corfu-popupinfo-mode)
   :custom
-  (corfu-auto nil)
+  (corfu-auto t)
+  (corfu-auto-delay 0.5)
   (corfu-quit-no-match t)
   (corfu-min-width 30)
   (corfu-popupinfo-delay nil)
   :bind
   (:map corfu-map
-	("SPC" . corfu-insert-separator)
+	("M-SPC" . corfu-insert-separator)
 	("<f1>" . corfu-popupinfo-toggle)
 	("<f2>" . corfu-popupinfo-location))
   :init
   (global-corfu-mode))
 
-(use-package corfu-candidate-overlay
-  :after corfu
-  :config
-  (corfu-candidate-overlay-mode))
-
+;; Quick custom completions
 (use-package abbrev
   :ensure nil
   :diminish
@@ -206,39 +234,38 @@
 (use-package org
   :config
   (advice-add 'org-agenda-quit :before 'org-save-all-org-buffers)
-  
   :hook
   ('org-mode . 'visual-line-mode)
-  
   :custom
   (org-return-follows-link t)
-  
   (org-todo-keywords
    '((sequence
       "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
-  
   (org-agenda-files (list org-directory))
   (org-archive-location
    (concat (file-name-as-directory org-directory) "archive/archive23.org::"))
-
   (org-refile-targets
 	'((nil :maxlevel . 9)
           (org-agenda-files :maxlevel . 9)))
   (org-outline-path-complete-in-steps nil)
   (org-refile-use-outline-path t)
-  
   (org-capture-templates '(("t" "Task" entry
 			    (file "inbox.org")
 			    "* TODO %i%?")
 			   ("a" "Appointment" entry
 			    (file+headline "calendar.org" "Appointments")
-			    "* %i%? \n %^t")))
-
+			    "* %i%? \n %^t")
+			   ("n" "New note (with Denote)" plain
+			    (file denote-last-path)
+			    #'denote-org-capture
+			    :no-save t
+			    :immediate-finish nil
+			    :kill-buffer t
+			    :jump-to-captured t)))
   (org-agenda-remove-tags t)
   (org-agenda-custom-commands '(("u" "School" tags-todo "ubc")
 				("r" "Raincoast" tags-todo "raincoast")
 				("p" "Personal" tags-todo "personal")))
-  
   ;; Here I define the statutory holidays for British Columbia
   ;; which will appear in my calendar views.
   (holiday-local-holidays
@@ -255,7 +282,6 @@
 	  (holiday-fixed 11 11 "Rememberance Day")
 	  (holiday-float 11 4 4 "American Thanksgiving")
 	  (holiday-fixed 12 25 "Christmas")))
-
   ;; turn off all the US and religeous holidays
   (holiday-general-holidays nil)
   (holiday-christian-holidays nil)
@@ -263,11 +289,8 @@
   (holiday-bahai-holidays nil)
   (holiday-hebrew-holidays nil)
   (holiday-oriental-holidays nil)
-
   (calendar-holidays holiday-local-holidays)
-
   (fill-prefix "") ; fixes some bug with pressing RET in org buffers.
-  
   :bind
   ("C-c a" . 'org-agenda)
   ("C-c c" . 'org-capture)
@@ -282,8 +305,7 @@
 ;; -----------------------------------------------------------------------------
 ;; Programming
 
-;; The following two packages provide linting and documentation on hover
-
+;; On-the-fly linter
 (use-package flycheck
   :hook
   ('ess-mode-hook . flycheck-mode)
@@ -297,6 +319,7 @@
   ("C-c f n" . 'flycheck-next-error)
   ("C-c f p" . 'flycheck-previous-error))
 
+;; Documentation on hover
 (use-package eldoc
   :diminish
   :custom
@@ -305,54 +328,44 @@
 
 
 ;; -----------------------------------------------------------------------------
-;; Programming Languages
-
 ;; R
-(defalias 'dh/ess-insert-pipe
-   (kmacro "SPC % > C-e <return>"))
+
+;; TODO: `ess-r-mode-map' is not defined in polymode. How can I use my ESS
+;;       keybindings in polymode (e.g., for assignment or pipes)?
+
+;; (defalias 'dh/ess-insert-pipe
+;;    (kmacro "SPC % > C-e <return>"))
 
 (use-package ess
   :custom
   (ess-use-flymake nil)
   (inferior-R-args "--no-save")
   (ess-R-font-lock-keywords
-   '((ess-R-fl-keywor~modifiers . t)
-     (ess-R-fl-keywor~fun-defs . t)
-     (ess-R-fl-keywor~keywords . t)
-     (ess-R-fl-keywor~assign-ops . t)
-     (ess-R-fl-keywor~constants . t)
-     (ess-fl-keywor~fun-calls . t)
-     (ess-fl-keywor~numbers . t)
-     (ess-fl-keywor~operators . t)
-     (ess-fl-keywor~delimiters)
-     (ess-fl-keywor~=)
-     (ess-R-fl-keywor~F&T . t)
-     (ess-R-fl-keywor~%op% . t)))
-  :bind
-  (:map ess-mode-map
-	("C-," . 'ess-insert-assign)
-	("C-." . 'dh/ess-insert-pipe))
+   '((ess-R-fl-keyword:modifiers . t)
+     (ess-R-fl-keyword:fun-defs . t)
+     (ess-R-fl-keyword:keywords . t)
+     (ess-R-fl-keyword:assign-ops . t)
+     (ess-R-fl-keyword:constants . t)
+     (ess-fl-keyword:fun-calls . t)
+     (ess-fl-keyword:numbers)
+     (ess-fl-keyword:operators)
+     (ess-fl-keyword:delimiters)
+     (ess-fl-keyword:=)
+     (ess-R-fl-keyword:F&T . t)
+     (ess-R-fl-keyword:%op% . t)))
+  ;; :bind
+  ;; (:map ess-r-mode-map
+  ;; 	("C-," . ess-insert-assign)
+  ;; 	("C-." . dh/ess-insert-pipe))
   :hook
   ('ess-mode-hook 'turn-on-pretty-mode)
   ('inferior-ess-mode-hook ; fixes color bug in console
    setq-local ansi-color-for-comint-mode 'filter))
 
-;; Python
-(use-package pyvenv)
 
-;; Julia
-(use-package julia-mode)
-
-;; Go
-(use-package go-mode)
-
-;; J
-(use-package j-mode
-  :custom
-  (j-console-cmd "~/J/j9.4/jconsole.sh")
-  :mode ("\\.ij[rstp]$" . j-mode))
-
+;; -----------------------------------------------------------------------------
 ;; Common Lisp
+
 ;; slime-helper.el takes a few seconds to load, so I bind it to a command
 ;; instead of loading it on startup.
 
@@ -364,8 +377,9 @@
 
 
 ;; -----------------------------------------------------------------------------
-;; Writing
+;; Writing tools (Note-taking and Markdown documents)
 
+;; On-the-fly spell checker
 (use-package flyspell
   :ensure nil
   :diminish
@@ -374,6 +388,7 @@
   ('Rd-mode-hook 'flyspell-mode)
   ('org-mode-hook 'flyspell-mode))
 
+;; Spell checking tool
 (use-package ispell
   :after (flyspell)
   :ensure nil
@@ -385,6 +400,7 @@
   :bind
   ("<f5>" . 'ispell-word))
 
+;; Citation manager (integrates with Zotero via better-bibtex)
 (use-package citar
   :custom
   (citar-bibliography '("~/Zotero/references.bib"))
@@ -400,13 +416,12 @@
   (org-mode . citar-capf-setup)
   (markdown-mode . citar-capf-setup))
 
+;; Notes management (implements custom file-naming format for organization)
 (use-package denote
-  ;; quickly take notes in markdown format.
   :custom
   (denote-directory (file-name-as-directory "~/Notes"))
   (denote-silo-extras--directories '("~/Notes/work" "~/Notes/chess"))
   (denote-file-type 'markdown-yaml)
-  ;; (denote-file-type nil) ; defaults to org
   (denote-infer-keywords t)
   (denote-sort-keywords t)
   (denote-prompts '(title keywords))
@@ -421,18 +436,21 @@
   ("C-c d F" . denote-find-backlink)
   ("C-c d r" . denote-rename-file))
 
+;; Edit markdown documents
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode))
   :custom
   (markdown-command "pandoc"))
 
+;; Edit Rmarkdwon documents
 (use-package poly-R
   :mode ("\\.[rR]md\\'" . poly-gfm+r-mode)
   :custom
   (markdown-code-block-braces t)
   (markdown-asymmetric-header t))
 
+;; Cleanup hooks when opening markdown files
 (add-hook 'markdown-mode-hook #'visual-line-mode)
 (add-hook 'find-file-hook #'denote-link-buttonize-buffer)
 
@@ -440,12 +458,13 @@
 ;; -----------------------------------------------------------------------------
 ;; PDF and EPUB
 
+;; Built-in PDF reader
 (use-package pdf-tools
-  :defines pdf-view-themed-minor-mode
   :mode ("\\.pdf\\'" . pdf-view-mode)
   :hook
   ('pdf-view-mode-hook  . 'pdf-view-themed-minor-mode))
 
+;; Built in EPUB reader
 (use-package nov
   :mode
   (("\\.epub\\'" . nov-mode))
@@ -453,49 +472,18 @@
   (nov-unzip-program "tar")
   (nov-unzip-args '("-xC" directory "-f" filename)))
 
-
 ;; -----------------------------------------------------------------------------
-;; Eshell
+;; Start-up profiling
 
-;; Note: I don't really use eshell anymore since 'vterm' is wayyy better.
+;; See the start-up timer in `early-init.el'
 
-;; This is necessary since the 'clear' command sends any current input
-;; on the command line before clearing for some reason. The following
-;; code renders the expected C-l behavior of most shells.
-
-;; (declare-function eshell-kill-input 'eshell)
-;; (declare-function eshell-send-input 'eshell)
-;; (declare-function eshell-bol        'eshell)
-
-;; (defun dh/run-this-in-eshell (cmd)
-;;     "Run the command 'CMD' in eshell."
-;;     `(goto-char (point-max))
-;;     (eshell-kill-input)
-;;     (message (concat "Running in Eshell: " cmd))
-;;     (insert cmd)
-;;     (eshell-send-input)
-;;     `(goto-char (point-max))
-;;     (eshell-bol)
-;;     (yank))
-
-;; (add-hook 'eshell-mode-hook
-;; 	  (lambda ()
-;; 	    (interactive)
-;; 	    (bind-key* "C-l" (dh/run-this-in-eshell "clear 1") eshell-mode-map)))
-
-
-;; -----------------------------------------------------------------------------
-;; Start up time
-
-;; Esup provides start up time profiling. See also the start up timer message
-;; function in early-init.el.
-
+;; ;; Profile start-up time on a per-package basis
 ;; (use-package esup
 ;;     :custom
 ;;     (esup-depth 0))
 
-;; The garbage collection threshold is set very low in early-init to reduce
-;; start up time. Here we return it to a reasonable value.
+;; The garbage collection threshold is set very low in `early-init.el' to reduce
+;; start-up time. Here we return it to a reasonable value.
 (setq gc-cons-threshold (* 2 1000 1000))
 
 ;;; init.el ends here
