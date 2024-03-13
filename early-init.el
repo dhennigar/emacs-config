25a39cb1ac92e6adfbcb1ce8f7de4263f603d22a
dhennigar <drhenn@proton.me>
2024-03-13 00:06:00 -0700

    update early init

--- early-init.el
+++ early-init.el
@@ -81,6 +81,25 @@
 (scroll-bar-mode -1)
 (setq menu-bar-mode nil)
 
+;; Default mode-line
+(setq-default mode-line-format
+	      '("%e"
+		mode-line-front-space
+		(:propertize (""
+			      mode-line-mule-info
+			      mode-line-client mode-line-modified mode-line-remote)
+			     display (min-width (5.0)))
+		mode-line-frame-identification
+		mode-line-buffer-identification
+		"   "
+		mode-line-position
+		(vc-mode vc-mode)
+		"  "
+		mode-line-modes
+		mode-line-misc-info
+		mode-line-eradio
+		mode-line-end-spaces))
+
 ;; Smoothe scrolling
 (pixel-scroll-precision-mode)
 (setq pixel-scroll-precision-use-momentum t)
